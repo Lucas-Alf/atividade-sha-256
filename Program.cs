ec9089d8524966cc8b8e50d9f3a9e471d4c3d03e
Lucas-Alf <lucasmachadoalf@gmail.com>
2020-09-24 21:22:31 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atividade_sha_256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
